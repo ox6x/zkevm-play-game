991a80596b0379780a556da28c01bd39066d0c17
ox6x <ox6.x@proton.me>
2024-12-20 05:58:32 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,15 @@ import { BinanceTestnet } from "@thirdweb-dev/chains";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 
+// 从 .env 文件中加载环境变量
+const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       activeChain={BinanceTestnet}
-      clientId={process.env.NEXT_PUBLIC_CLIENT_ID} // 从 .env 文件中获取
+      // 强制断言 clientId 的类型为 string
+      clientId={clientId as string}
     >
       <ChakraProvider>
         <NavBar />
@@ -19,4 +23,3 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
-
