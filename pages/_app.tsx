f3fb86481633e9accf5df741372018f072b9f90f
ox6x <ox6.x@proton.me>
2024-12-20 04:43:54 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,14 @@ import { BinanceTestnet } from "@thirdweb-dev/chains";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-// const activeChain = "PolygonZkevmTestnet";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={BinanceTestnet}>
+    <ThirdwebProvider
+      activeChain={BinanceTestnet}
+      clientId={process.env.NEXT_PUBLIC_CLIENT_ID} // 从 .env 文件中获取
+    >
       <ChakraProvider>
-        <NavBar/>
+        <NavBar />
         <Component {...pageProps} />
       </ChakraProvider>
     </ThirdwebProvider>
@@ -21,3 +19,4 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
+
