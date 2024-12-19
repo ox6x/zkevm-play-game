32cef311704ae2bdd178712073699915804e49b9
ox6x <ox6.x@proton.me>
2024-12-20 05:51:46 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { binance-testnet } from "@thirdweb-dev/chains";
+import { BinanceTestnet } from "@thirdweb-dev/chains";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      activeChain={binance-testnet}
+      activeChain={BinanceTestnet}
       clientId={process.env.NEXT_PUBLIC_CLIENT_ID} // 从 .env 文件中获取
     >
       <ChakraProvider>
