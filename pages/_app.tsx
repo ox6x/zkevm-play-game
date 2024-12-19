4b05939c60dace26b256f5ca71044f141d0966ad
ox6x <ox6.x@proton.me>
2024-12-20 06:15:48 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,11 @@ import { BinanceTestnet } from "@thirdweb-dev/chains";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 
-// 从 .env 文件中加载环境变量
-const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider
-      activeChain={BinanceTestnet}
-      // 强制断言 clientId 的类型为 string
-      clientId={clientId as string}
-    >
+    <ThirdwebProvider activeChain={BinanceTestnet}>
       <ChakraProvider>
-        <NavBar />
+        <NavBar/>
         <Component {...pageProps} />
       </ChakraProvider>
     </ThirdwebProvider>
