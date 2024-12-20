c9f67935cd215c01113f3e7c7f74007b2d288085
ox6x <ox6.x@proton.me>
2024-12-20 11:44:41 +0700

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import HomeComponent from "../../components/Home";
+import HomeComponent from "../components/Home";
 
 export default function HomePage() {
   return <HomeComponent />;
