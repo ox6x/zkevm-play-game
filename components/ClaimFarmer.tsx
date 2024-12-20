69f1f28ee9fbd769374ad29ca2ccce3724f1fe32
ox6x <ox6.x@proton.me>
2024-12-20 09:18:10 +0700

    Add files via upload

--- components/ClaimFarmer.tsx
+++ components/ClaimFarmer.tsx
@@ -1,33 +1,11 @@
-import { MediaRenderer, useContract, useContractMetadata, useAddress } from "@thirdweb-dev/react";
-import { useSendTransaction } from "thirdweb/react";
-import { claimTo } from "thirdweb/extensions/erc1155";
+import { MediaRenderer, Web3Button, useContract, useContractMetadata } from "@thirdweb-dev/react";
 import { FARMER_ADDRESS } from "../const/addresses";
-import { Box, Container, Flex, Heading, Button } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading } from "@chakra-ui/react";
 
 export function ClaimFarmer() {
     const { contract } = useContract(FARMER_ADDRESS);
     const { data: metadata } = useContractMetadata(contract);
-    const address = useAddress();
-    const { mutate: sendTransaction } = useSendTransaction();
-
-    const onClick = () => {
-        if (!address) {
-            alert("请先连接钱包");
-            return;
-        }
-        if (!contract) {
-            alert("合约未找到");
-            return;
-        }
-        const transaction = claimTo({
-            contract,
-            to: address,
-            amount: 1,
-            tokenId: 0,
-        });
-        sendTransaction(transaction);
-    };
-
+    
     return (
         <Container maxW={"1200px"}>
             <Flex direction={"column"} alignItems={"center"} justifyContent={"center"} h={"50vh"}>
@@ -40,7 +18,10 @@ export function ClaimFarmer() {
                     />
                 </Box>
                 
-                <Button onClick={onClick}>Claim Farmer</Button>
+                <Web3Button
+                    contractAddress={FARMER_ADDRESS}
+                    action={(contract) => contract.erc1155.claim(0, 1)}
+                >Claim Farmer</Web3Button>
             </Flex>
         </Container>
     );
