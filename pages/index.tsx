3e041de82d5fb65e853c36984c6ff2d156e5d3a4
ox6x <ox6.x@proton.me>
2024-12-20 10:43:19 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,108 +1,44 @@
-import { ConnectWallet, MediaRenderer, useAddress, useContract, useContractRead, useOwnedNFTs } from "@thirdweb-dev/react";
-import type { NextPage } from "next";
-import { FARMER_ADDRESS, REWARDS_ADDRESS, STAKING_ADDRESS, TOOLS_ADDRESS } from "../const/addresses";
-import { ClaimFarmer } from "../components/ClaimFarmer";
-import { Inventory } from "../components/Inventory";
-import { Equipped } from "../components/Equipped";
-import { BigNumber, ethers } from "ethers";
-import { Text, Box, Card, Container, Flex, Heading, SimpleGrid, Spinner, Skeleton } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { Login } from "./Login"; // 导入 Login 组件
+import { Farmer } from "./Farmer"; // 导入 Farmer 组件
+import { Home } from "./Home"; // 导入 Home 组件
+import { useAddress, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
+import { FARMER_ADDRESS } from "../const/addresses";
 
-const Home: NextPage = () => {
-  const address = useAddress();
+const Index: React.FC = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [hasPass, setHasPass] = useState(false);
+    const address = useAddress(); // 检查是否连接了钱包
+    const { contract } = useContract(FARMER_ADDRESS);
+    const { data: ownedNFTs } = useOwnedNFTs(contract, address);
 
-  const { contract: farmercontract } = useContract(FARMER_ADDRESS);
-  const { contract: toolsContract } = useContract(TOOLS_ADDRESS);
-  const { contract: stakingContract } = useContract(STAKING_ADDRESS);
-  const { contract: rewardContract } = useContract(REWARDS_ADDRESS);
+    // 检查是否已连接钱包
+    useEffect(() => {
+        if (address) {
+            setIsLoggedIn(true);
+        } else {
+            setIsLoggedIn(false);
+        }
+    }, [address]);
 
-  const { data: ownedFarmers, isLoading: loadingOwnedFarmers } = useOwnedNFTs(farmercontract, address);
-  const { data: ownedTools, isLoading: loadingOwnedTools } = useOwnedNFTs(toolsContract, address);
+    // 检查用户是否拥有通行证
+    useEffect(() => {
+        if (ownedNFTs && ownedNFTs.length > 0) {
+            setHasPass(true);
+        } else {
+            setHasPass(false);
+        }
+    }, [ownedNFTs]);
 
-  const { data: equippedTools } = useContractRead(
-    stakingContract, 
-    "getStakeInfo",
-    [address]
-  );
+    if (!isLoggedIn) {
+        return <Login />;
+    }
 
-  const { data: rewardBalance } = useContractRead(rewardContract, "balanceOf", [address]);
-  
-  if (!address) {
-    return (
-      <Container maxW={"1200px"}>
-        <Flex direction={"column"} h={"100vh"} justifyContent={"center"} alignItems={"center"}>
-          <Heading my={"40px"}>Welcom to Crypto Farm</Heading>
-          <ConnectWallet />
-        </Flex>
-      </Container>
-    );
-  }
+    if (!hasPass) {
+        return <Farmer />;
+    }
 
-  if (loadingOwnedFarmers) {
-    return(
-      <Container maxW={"1200px"}>
-        <Flex h={"100vh"} justifyContent={"center"} alignItems={"center"}>
-          <Spinner />
-        </Flex>
-      </Container>
-    );
-  }
-
-  if (ownedFarmers?.length === 0) {
-    return (
-      <Container maxW={"1200px"}>
-        <ClaimFarmer />
-      </Container>
-    );
-  }
-
-  return (
-    <Container maxW={"1200px"}>
-      <SimpleGrid columns={2} spacing={10}>
-        <Card p={5}>
-          <Heading>Farmer:</Heading>
-          <SimpleGrid columns={2} spacing={10}>
-            <Box>
-              {ownedFarmers?.map((nft) => (
-                <div key={nft.metadata.id}>
-                  <MediaRenderer 
-                    src={nft.metadata.image} 
-                    height="100%"
-                    width="100%"
-                  />
-                </div>
-              ))}
-            </Box>
-            <Box>
-              <Text fontSize={"small"} fontWeight={"bold"}>$CARROT Balance:</Text>
-                {rewardBalance && (
-                    <p>{ethers.utils.formatUnits(rewardBalance, 18)}</p>
-                  )}
-              </Box>
-          </SimpleGrid>
-        </Card>
-        <Card p={5}>
-          <Heading>Inventory:</Heading>
-          <Skeleton isLoaded={!loadingOwnedTools}>
-            <Inventory
-              nft={ownedTools}
-            />     
-          </Skeleton>
-        </Card>
-      </SimpleGrid>
-      <Card p={5} my={10}>
-        <Heading mb={"30px"}>Equiped Tools:</Heading>
-        <SimpleGrid columns={3} spacing={10}>
-            {equippedTools &&
-              equippedTools[0].map((nft: BigNumber) => (
-                <Equipped
-                  key={nft.toNumber()}
-                  tokenId={nft.toNumber()}
-                />
-              ))}
-        </SimpleGrid>
-      </Card>
-    </Container>
-  );
+    return <Home />;
 };
 
-export default Home;
+export default Index;
